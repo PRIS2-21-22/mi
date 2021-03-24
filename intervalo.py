96229b08ed2f94ba0032d07df82b906cc84518b4
Dor494 <dor494@inlumine.ual.es>
2021-03-24 17:51:34 +0100

    Version 1.1

--- intervalo.py
+++ intervalo.py
@@ -13,22 +13,22 @@ class InverseInterval(Exception):
     pass
 
 class Interval(object):
-    def __init__(self, start, end):
-        if isinstance(start, date) and isinstance(end, date):
+    def __init__(self, inicio, fin):
+        if isinstance(inicio, date) and isinstance(fin, date):
             self.mode = "DATE"
-        elif isinstance(start, datetime) and isinstance(end, datetime):
+        elif isinstance(inicio, datetime) and isinstance(fin, datetime):
             self.mode = "DATETIME"
         else:
             raise IncorrectType("Both start and end of interval must be same date/datetime type.")
 
-        if start == end:
+        if inicio == fin:
             raise IntervalLengthCannotBeZero("Length of interval cannot be zero.")
 
-        if start > end:
+        if inicio > fin:
             raise InverseInterval("Start of interval must be before end of interval.")
 
-        self.start = start
-        self.end = end
+        self.inicio = inicio
+        self.fin = fin
         
     def preceeds(self, other):
         """
@@ -36,7 +36,7 @@ class Interval(object):
         Boolean Expression: 
         This interval is before the other interval.
         """
-        return (self.start < self.end < other.start)
+        return (self.inicio < self.fin < other.inicio)
     
     def meets(self, other):
         """
@@ -45,7 +45,7 @@ class Interval(object):
         This interval starts before the interval and ends at the start of
         the other interval.
         """
-        return (self.start < self.end == other.start)
+        return (self.inicio < self.fin == other.inicio)
 
     def overlaps(self, other):
         """
@@ -53,7 +53,7 @@ class Interval(object):
         Boolean Expression: 
         This interval overlaps the other interval.
         """
-        return (self.start <= other.end and other.start <= self.end)
+        return (self.inicio <= other.fin and other.inicio <= self.fin)
 
     def finished_by(self, other):
         """
@@ -61,7 +61,7 @@ class Interval(object):
         Boolean Expression: 
         This interval ends at the same time as the other interval.
         """
-        return (self.start <= other.start and self.end == other.end)
+        return (self.inicio <= other.inicio and self.fin == other.fin)
 
     def contains(self, other):
         """
@@ -69,7 +69,7 @@ class Interval(object):
         Boolean Expression: 
         This interval starts before and ends after the other interval.
         """
-        return (self.start <= other.start and self.end >= other.end)
+        return (self.inicio <= other.inicio and self.fin >= other.end)
     
     def starts(self, other):
         """
@@ -78,7 +78,7 @@ class Interval(object):
         This interval starts at the same time as the other interval starts and ends 
         before the other interval ends.
         """
-        return (self.start == other.start and self.end <= other.start)
+        return (self.inicio == other.inicio and self.fin <= other.inicio)
 
     def equals(self, other):
         """
@@ -86,7 +86,7 @@ class Interval(object):
         Boolean Expression: 
         This interval is identical to the other interval.
         """
-        return (self.start == other.start and self.end == other.end)
+        return (self.inicio == other.inicio and self.fin == other.fin)
 
     def started_by(self, other):
         """
@@ -95,7 +95,7 @@ class Interval(object):
         This interval starts when the other interval starts and ends after the 
         other interval ends.
         """
-        return (self.start == other.start and self.end >= other.end)
+        return (self.inicio == other.inicio and self.fin >= other.fin)
 
     def during(self, other):
         """
@@ -104,7 +104,7 @@ class Interval(object):
         This interval starts after the other interval starts and ends before
         the other interval ends.
         """
-        return (self.start >= other.start and self.end <= other.end)
+        return (self.inicio >= other.inicio and self.fin <= other.fin)
 
     def finishes(self, other):
         """
@@ -113,7 +113,7 @@ class Interval(object):
         This interval starts after the other interval starts and ends when the other
         interval finishes.
         """
-        return (self.start <= self.end == other.end)
+        return (self.inicio <= self.fin == other.fin)
     
     def overlapped_by(self, other):
         """
@@ -122,7 +122,7 @@ class Interval(object):
         This interval starts after the other interval starts but before the other interval
         ends, and ends after the other interval ends.
         """
-        return (other.start <= self.start <= other.end and self.end >= other.start)
+        return (other.inicio <= self.inicio <= other.fin and self.fin >= other.inicio)
 
     def met_by(self, other):
         """
@@ -130,7 +130,7 @@ class Interval(object):
         Boolean Expression: 
         This interval starts when the other interval ends.
         """
-        return (other.end == self.start and self.end > other.end)
+        return (other.fin == self.inicio and self.fin > other.fin)
 
     def preceded_by(self, other):
         """
@@ -138,7 +138,7 @@ class Interval(object):
         Boolean Expression: 
         The interval starts after the other interval ends.
         """
-        return (other.end < self.start)
+        return (other.fin < self.inicio)
 
     # Operations
     def intersection(self, other):
@@ -146,7 +146,7 @@ class Interval(object):
         Returns the intersection of this interval and the other interval.
         """
         if self.overlaps(other) and not (self.meets(other) or self.met_by(other)):
-            return Interval(max(self.start, other.start), min(self.end, other.end))
+            return Interval(max(self.inicio, other.inicio), min(self.fin, other.fin))
         else:
             return None
 
@@ -154,15 +154,15 @@ class Interval(object):
         """
         Returns the union of this interval and the other interval.
         """
-        return Interval(min(self.start, other.start), min(self.end, other.end))
+        return Interval(min(self.inicio, other.inicio), min(self.fin, other.fin))
 
     def subtract(self, other):
         """
         Returns and interval with the other interval subtracted from this interval.
         """
         if self.overlaps(other):
-            start_datetime = self.start
-            end_datetime = other.start
+            start_datetime = self.inicio
+            end_datetime = other.inicio
         elif self.overlapped_by(other):
             start_datetime = 2
 
@@ -171,7 +171,7 @@ class Interval(object):
     # Computed Properties
     @property
     def duration(self):
-        return (self.end - self.start)
+        return (self.fin - self.inicio)
 
     @property
     def seconds(self):
@@ -203,9 +203,9 @@ class Interval(object):
         return self.union(other)
     
     def __str__(self):
-        return "<Interval: Start: {}, End: {}>".format(self.start.isoformat(), self.end.isoformat())
+        return "<Interval: Start: {}, End: {}>".format(self.inicio.isoformat(), self.fin.isoformat())
     
     def __repr__(self):
-        return "<Interval: Start: {}, End: {}>".format(self.start.isoformat(), self.end.isoformat())
+        return "<Interval: Start: {}, End: {}>".format(self.inicio.isoformat(), self.fin.isoformat())
 
 print("Intervalo")
